90d08fc90839a2268860c6380041f2a4472cc6f5
Bensirpent07 <benkuhman@gmail.com>
2026-09-25 22:44:16 -0400

    Add CatalogItem validation

---
+++ tests/catalog/OpenLend.Catalog.UnitTests/CatalogItems/CatalogItemTests.cs
@@ -0,0 +1,24 @@
+﻿using OpenLend.Catalog.Domain.CatalogItems;
+
+namespace OpenLend.Catalog.UnitTests.CatalogItems;
+public sealed class CatalogItemTests
+{
+    [Fact]
+    public void Constructor_WhenNameExceedsMaximumLength_ThrowsArgumentException()
+    {
+        var name = new string('A', 201);
+        CatalogItem Act() => new(name);
+        var exception = Assert.Throws<ArgumentException>(Act);
+        Assert.Equal("name", exception.ParamName);
+    }
+
+    [Fact]
+    public void Constructor_WhenDescriptionExceedsMaximumLength_ThrowsArgumentException()
+    {
+        var name = "Valid Name";
+        var description = new string('A', 2001);
+        CatalogItem Act() => new(name, description);
+        var exception = Assert.Throws<ArgumentException>(Act);
+        Assert.Equal("description", exception.ParamName);
+    }
+}
